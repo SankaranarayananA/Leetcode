aa7d78ef8c9f8245b2f092b45a92d325c3970d9d
SankaranarayananA <76698114+SankaranarayananA@users.noreply.github.com>
2022-09-09 01:22:06 +0530

    Update 217. Contains Duplicate.cpp

--- Array/Easy/217. Contains Duplicate.cpp
+++ Array/Easy/217. Contains Duplicate.cpp
@@ -3,10 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> mp;
         for(int x: nums){
+            if(mp.find(x)!= mp.end())return true;
             mp[x]++;
-        }
-        for(auto x: mp){
-            if(x.second>=2) return true;
         }
         return false;
     }
